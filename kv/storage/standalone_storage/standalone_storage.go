4d55efb8e5db54542995f81568459c4209308bd9
Yuxubinchen <2302370185@qq.com>
2021-11-20 22:17:59 +0800

    update lab1 write and read

--- kv/storage/standalone_storage/standalone_storage.go
+++ kv/storage/standalone_storage/standalone_storage.go
@@ -3,22 +3,31 @@ package standalone_storage
 import (
 	"github.com/pingcap-incubator/tinykv/kv/config"
 	"github.com/pingcap-incubator/tinykv/kv/storage"
+	"github.com/pingcap-incubator/tinykv/kv/util/engine_util"
 	"github.com/pingcap-incubator/tinykv/proto/pkg/kvrpcpb"
 )
 
 // StandAloneStorage is an implementation of `Storage` for a single-node TinyKV instance. It does not
 // communicate with other nodes and all data is stored locally.
 type StandAloneStorage struct {
-	// Your Data Here (1).
+	DBPath string // Directory to store the data in. Should exist and be writable.
+	raft   bool
+	en     *engine_util.Engines
+	batch  *engine_util.WriteBatch
+	badgeread DBIterator
 }
 
 func NewStandAloneStorage(conf *config.Config) *StandAloneStorage {
-	// Your Code Here (1).
-	return nil
+	return &StandAloneStorage{
+		DBPath:    conf.DBPath,
+		raft:      conf.Raft,
+		en:        nil,
+		batch:     new(engine_util.WriteBatch),
+		badgeread: engine_util.DBIterator
+	}
 }
-
 func (s *StandAloneStorage) Start() error {
-	// Your Code Here (1).
+	s.en.Kv = engine_util.CreateDB(s.DBPath, s.raft)
 	return nil
 }
 
@@ -28,11 +37,39 @@ func (s *StandAloneStorage) Stop() error {
 }
 
 func (s *StandAloneStorage) Reader(ctx *kvrpcpb.Context) (storage.StorageReader, error) {
-	// Your Code Here (1).
+
+	if ctx == nil {
+		return nil, nil
+	}
+
 	return nil, nil
 }
 
 func (s *StandAloneStorage) Write(ctx *kvrpcpb.Context, batch []storage.Modify) error {
-	// Your Code Here (1).
+
+	for _, pt := range batch {
+		switch pt.Data.(type) {
+		case storage.Put:
+			s.batch.SetCF(engine_util.CfDefault, pt.Key(), pt.Value())
+		case storage.Delete:
+			s.batch.DeleteCF(engine_util.CfDefault, pt.Key())
+		}
+	}
+	err := s.batch.WriteToDB(s.en.Kv)
+	return err
+}
+func (s *storage.StorageReader) GetCF(cf string, key []byte) ([]byte, error) {
+	if key==nil{
+		return nil,nil
+	}
+	return engine_util.GetCF(s.en.Kv, engine_util.CfDefault, pt.Key())
+}
+func (s *storage.StorageReader) IterCF(cf string) engine_util.DBIterator {
+    txn := s.en.Kv.
+	defaultIter := engine_uengine_util.NewCFIterator 
+	defer txn.Discard()
 	return nil
+}
+func (s *storage.StorageReader) Close() {
+   
 }
